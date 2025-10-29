4752c79a77ab0d4c3b82925176400ff36af1929c
Firman dani <danifirman487@gmail.com>
2025-10-29 22:10:59 +0700

    Add files via upload

---
+++ Latihan 2 Struktur kondisi.py
@@ -0,0 +1,33 @@
+print ("Nama = firman dani")
+
+#Mengurutkan bilangan dari yang terkecil
+a = int(input("masukkan bilangan pertama :"))
+b = int(input("masukkan bilangan kedua :"))
+c = int(input("masukkan bilangan ketiga :"))
+d = int(input("masukkan bilangan keempat :"))
+
+
+bilangan = [a, b, c, d]
+
+bilangan.sort()
+
+print("urutan bilangan :", bilangan)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
